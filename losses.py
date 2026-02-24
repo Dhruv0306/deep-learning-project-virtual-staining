fix(losses): adjust identity lambda calculation and discriminator loss target

losses.py
@@ -86,7 +86,9 @@ class CycleGANLoss:
         self.fake_B_buffer = ReplayBuffer()
 
     def get_identity_lambda(self, epoch, total_epochs):
-        return self.lambda_identity * (0.95**epoch)
+        if epoch <= 0.55*total_epochs : 
+            return self.lambda_identity
+        return self.lambda_identity * (0.997**(epoch - 0.55*total_epochs))
 
     def generator_loss(self, real_A, real_B, G_AB, G_BA, D_A, D_B, epoch, total_epochs):
 
@@ -165,7 +167,7 @@ class CycleGANLoss:
     def discriminator_loss(self, D, real, fake, replay_buffer=None):
         # Real loss
         pred_real = D(real)
-        loss_real = self.criterion_GAN(pred_real, 0.9 * torch.ones_like(pred_real))
+        loss_real = self.criterion_GAN(pred_real, 0.95 * torch.ones_like(pred_real))
 
         # Fake loss
         fake_buffer = replay_buffer.push_and_pop(fake) if replay_buffer else fake